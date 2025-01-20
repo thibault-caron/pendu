14325add753d9ce010945c18314cd7267c0e786a
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-20 11:29:10 +0100

    feat: importe la liste de mots dans main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Auteurs : Lorenzo OTTAVIANI, Olivier PORTAL et Thibault CARON
+Date : 20/01/2025 11h25
+But du programme :
+    Créer un jeu de pendu en utilisant l'interface graphique PyGame
+Entrée :
+Sortie :
+"""
+
+import pygame
+
+fichier_mots = open("mots.txt")
+
+print(fichier_mots.read())
