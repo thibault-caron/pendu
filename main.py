acece47c4465bd4dc8d6c0a05fe0c881557f1752
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-20 15:55:25 +0100

    feat: ajout dico touches_alphabet et constantes couleurs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Auteurs : Lorenzo OTTAVIANI, Olivier PORTAL et Thibault CARON
-Date : 20/01/2025 11h25
+Date : 20/01/2025 15h48
 But du programme :
     Créer un jeu de pendu en utilisant l'interface graphique PyGame
 Entrée :
@@ -8,7 +8,22 @@ Sortie :
 """
 
 import pygame
+from pygame.locals import *
 
-fichier_mots = open("mots.txt")
+NOIR = (0, 0, 0)
+GRIS = (127, 127, 127)
+BLANC = (255, 255, 255)
+ROUGE = (255, 0, 0)
+VERT = (0, 255, 0)
+BLEU = (0, 0, 255)
+JAUNE = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+fichier_mots = open("mots.txt", "a")
+
+touches_alphabet = {K_a: "a", K_b: "b", K_c: "c", K_d: "d", K_e: "e", K_f: "f", K_g: "g", K_h: "h", K_i: "i", K_j: "j",
+                    K_k: "k", K_l: "l", K_m: "m", K_n: "n", K_o: "o", K_p: "p", K_q: "q", K_r: "r", K_s: "s", K_t: "t",
+                    K_u: "u", K_v: "v", K_w: "w", K_x: "x", K_y: "y", K_z: "z"}
 
 print(fichier_mots.read())
