bccdd40635382d0407f9d42334151823db02f09e
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-21 15:54:38 +0100

    refactor: réorganisation du fichier main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Auteurs : Lorenzo OTTAVIANI, Olivier PORTAL et Thibault CARON
-Date : 20/01/2025 15h48
+Date : 21/01/2025 15h53
 But du programme :
     Créer un jeu de pendu en utilisant l'interface graphique PyGame
 Entrée :
@@ -14,7 +14,7 @@ from pygame.locals import *
 pygame.init()
 pygame.font.init()
 
-# Dimensions de la fenêtre
+# Dimensions de la fenêtre (classe)
 LARGEUR, HAUTEUR = 1000, 700
 fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
 pygame.display.set_caption("Jeu du Pendu - Edition spéciale Jinx")
@@ -30,76 +30,77 @@ JAUNE = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-# Police
-font = pygame.font.Font(None, 60)
 
-############ Affichage des objets
+# AFFICHAGE DES OBJETS #
+
 # Ajouter une image de fond
-fond_ecran_img = pygame.image.load("image/fond_ecran.jpg")
-fond_ecran_img = pygame.transform.scale(fond_ecran_img, (LARGEUR, HAUTEUR))
+fond_ecran = pygame.image.load("image/fond_ecran.jpg")
+fond_ecran = pygame.transform.scale(fond_ecran, (LARGEUR, HAUTEUR))
 
-#Base
-potence_base_img = pygame.image.load("image/acier2.jpg")
-potence_base_img = pygame.transform.scale(potence_base_img, (200, 20))
+# Base
+potence_base = pygame.image.load("image/acier2.jpg")
+potence_base = pygame.transform.scale(potence_base, (200, 20))
 
 # Poteau vertical
-potence_vert_img = pygame.image.load("image/acier2.jpg")
-potence_vert_img = pygame.transform.scale(potence_vert_img, (20, 400))
+potence_verticale = pygame.image.load("image/acier2.jpg")
+potence_verticale = pygame.transform.scale(potence_verticale, (20, 400))
 
 # Poteau horizontal
-potence_horz_img = pygame.image.load("image/acier2.jpg")
-potence_horz_img = pygame.transform.scale(potence_horz_img, (200, 20))
+potence_horizontale = pygame.image.load("image/acier2.jpg")
+potence_horizontale = pygame.transform.scale(potence_horizontale, (200, 20))
 
 # Corde
-corde_img = pygame.image.load("image/corde.png")
-corde_img = pygame.transform.scale(corde_img, (100, 180))
+corde1 = pygame.image.load("image/corde.png")
+corde1 = pygame.transform.scale(corde1, (50, 90))
 
-corde2_img = pygame.image.load("image/corde2.png")
-corde2_img = pygame.transform.scale(corde2_img, (100, 180))
+corde2 = pygame.image.load("image/corde2.png")
+corde2 = pygame.transform.scale(corde2, (50, 90))
 
-# Le pendu => Jinx
+# Jinx
 jinx_tete = pygame.image.load("image/jinx_head.webp")
-jinx_tete = pygame.transform.scale(jinx_tete, (353/2, 1276/2))
+jinx_tete = pygame.transform.scale(jinx_tete, (353 / 4, 1276 / 4))
 
 jinx_corps = pygame.image.load("image/jinx_body.webp")
-jinx_corps = pygame.transform.scale(jinx_corps, (353/2, 1276/2))
+jinx_corps = pygame.transform.scale(jinx_corps, (353 / 4, 1276 / 4))
 
-jinx_bras_droit = pygame.image.load("image/jinx_right_arm.webp") 
-jinx_bras_droit = pygame.transform.scale(jinx_bras_droit, (353/2, 1276/2))
+jinx_bras_droit = pygame.image.load("image/jinx_right_arm.webp")
+jinx_bras_droit = pygame.transform.scale(jinx_bras_droit, (353 / 4, 1276 / 4))
 
 jinx_bras_gauche = pygame.image.load("image/jinx_left_arm.webp")
-jinx_bras_gauche = pygame.transform.scale(jinx_bras_gauche, (353/2, 1276/2))
+jinx_bras_gauche = pygame.transform.scale(jinx_bras_gauche, (353 / 4, 1276 / 4))
 
 jinx_jambe_droite = pygame.image.load("image/jinx_right_leg.webp")
-jinx_jambe_droite = pygame.transform.scale(jinx_jambe_droite, (353/2, 1276/2))
+jinx_jambe_droite = pygame.transform.scale(jinx_jambe_droite, (353 / 4, 1276 / 4))
 
 jinx_jambe_gauche = pygame.image.load("image/jinx_left_leg.webp")
-jinx_jambe_gauche = pygame.transform.scale(jinx_jambe_gauche, (353/2, 1276/2))
+jinx_jambe_gauche = pygame.transform.scale(jinx_jambe_gauche, (353 / 4, 1276 / 4))
+
 
 # Dessiner la potence
 def dessine_potence():
-    fenetre.blit(potence_base_img, (260, 430))
-    fenetre.blit(potence_vert_img, (350, 30))
-    fenetre.blit(corde_img, (500, 30))
-    fenetre.blit(potence_horz_img, (370, 30))
-    
+    fenetre.blit(potence_base, (260, 430))
+    fenetre.blit(potence_verticale, (350, 30))
+    fenetre.blit(corde1, (500, 30))
+    fenetre.blit(potence_horizontale, (370, 30))
+
 
 # Dessiner Jinx
-def dessine_jinx(stage):
-    if stage > 0:  # Tête
-        fenetre.blit(jinx_tete, (460, 115))
-    if stage > 1:  # Corps
-        fenetre.blit(jinx_corps, (460, 115))
-        fenetre.blit(corde2_img, (500, 30))
-        fenetre.blit(jinx_tete, (460, 115))
-    if stage > 2:  # Bras gauche
-        fenetre.blit(jinx_bras_droit, (460, 115))
-    if stage > 3:  # Bras droit
-        fenetre.blit(jinx_bras_gauche, (460, 115))
-    if stage > 4:  # Jambe gauche
-        fenetre.blit(jinx_jambe_droite, (460, 115))
-    if stage > 5:  # Jambe droite
-        fenetre.blit(jinx_jambe_gauche, (460, 115))
+def dessine_jinx(tour):
+    if tour > 0:  # Tête
+        fenetre.blit(jinx_tete, (480, 70))
+    if tour > 1:  # Corps
+        fenetre.blit(jinx_corps, (480, 70))
+        fenetre.blit(corde2, (500, 30))
+        fenetre.blit(jinx_tete, (480, 70))
+    if tour > 2:  # Bras gauche
+        fenetre.blit(jinx_bras_droit, (480, 70))
+    if tour > 3:  # Bras droit
+        fenetre.blit(jinx_bras_gauche, (480, 70))
+    if tour > 4:  # Jambe gauche
+        fenetre.blit(jinx_jambe_droite, (480, 70))
+    if tour > 5:  # Jambe droite
+        fenetre.blit(jinx_jambe_gauche, (480, 70))
+
 
 fichier_mots = "mots.txt"
 
@@ -113,25 +114,20 @@ FPS = pygame.time.Clock()
 
 def main():
     """"""
-    en_cours = True    
+    en_cours = True
     while en_cours:
-        
-        fenetre.blit(fond_ecran_img, (0, 0))
+
+        fenetre.blit(fond_ecran, (0, 0))
         dessine_potence()
-        
+
         for evenement in pygame.event.get():
             if evenement.type == pygame.QUIT:
                 en_cours = False
 
-            if evenement.type == pygame.KEYDOWN:
-                if evenement.key == pygame.K_r:
-                    couleur = ROUGE
-                elif evenement.key == pygame.K_v:
-                    couleur = VERT
-                    fenetre
-                elif evenement.key == pygame.K_1:
-                    police.render("Placeholder lance partie")
-            
+            # if evenement.type == pygame.KEYDOWN:
+                # if evenement.key == pygame.K_r:
+                    # couleur = ROUGE
+
         pygame.display.update()
         FPS.tick(60)
 
@@ -143,5 +139,3 @@ if __name__ == "__main__":  # Evite que le programme puisse être lancé depuis 
     # nouveau_mot = "tuyau"
     # ajouter_mot(fichier_mots, nouveau_mot)
     main()
-
-
