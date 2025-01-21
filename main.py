b4b56b69b19a775c17fbbf2fbf3e679586b4027a
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-21 10:22:21 +0100

    feat: ajoute fonction permettant d'ajouter des mots

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Sortie :
 """
 
 import pygame
+from fonctions.ajouter_mot import ajouter_mot
 from pygame.locals import *
 
 NOIR = (0, 0, 0)
@@ -20,10 +21,15 @@ JAUNE = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-fichier_mots = open("mots.txt", "a")
+fichier_mots = open("mots.txt")
 
 touches_alphabet = {K_a: "a", K_b: "b", K_c: "c", K_d: "d", K_e: "e", K_f: "f", K_g: "g", K_h: "h", K_i: "i", K_j: "j",
                     K_k: "k", K_l: "l", K_m: "m", K_n: "n", K_o: "o", K_p: "p", K_q: "q", K_r: "r", K_s: "s", K_t: "t",
                     K_u: "u", K_v: "v", K_w: "w", K_x: "x", K_y: "y", K_z: "z"}
 
 print(fichier_mots.read())
+
+
+if __name__ == "__main__":  # Evite que le programme puisse être lancé depuis un autre programme.
+    nouveau_mot = "tuyau"
+    ajouter_mot(fichier_mots, nouveau_mot)
