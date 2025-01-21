43e6973b5723bda0d669dfc36416d6a3611dad57
Thibault Caron <thibault.caron@laplateforme.io>
2025-01-21 10:43:05 +0100

    fix: fix var fichier_mot
    (remove 'open()' from it)  so that ajouter_mots work

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ JAUNE = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-fichier_mots = open("mots.txt")
+fichier_mots = "mots.txt"
 
 touches_alphabet = {K_a: "a", K_b: "b", K_c: "c", K_d: "d", K_e: "e", K_f: "f", K_g: "g", K_h: "h", K_i: "i", K_j: "j",
                     K_k: "k", K_l: "l", K_m: "m", K_n: "n", K_o: "o", K_p: "p", K_q: "q", K_r: "r", K_s: "s", K_t: "t",
                     K_u: "u", K_v: "v", K_w: "w", K_x: "x", K_y: "y", K_z: "z"}
 
-print(fichier_mots.read())
+print(open(fichier_mots).read())
 
 
 if __name__ == "__main__":  # Evite que le programme puisse être lancé depuis un autre programme.
