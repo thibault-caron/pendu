d85187adbe9cd48e4ba2d676f76e339e7bde3e5d
MrRobot <olivier.portal@laplateforme.io>
2025-01-22 15:59:49 +0100

    style: remove unused dict

--- main.py
+++ main.py
@@ -198,10 +198,6 @@ def bouton_arreter():
 
 fichier_mots = "mots.txt"
 
-touches_alphabet = {K_a: "a", K_b: "b", K_c: "c", K_d: "d", K_e: "e", K_f: "f", K_g: "g", K_h: "h", K_i: "i", K_j: "j",
-                    K_k: "k", K_l: "l", K_m: "m", K_n: "n", K_o: "o", K_p: "p", K_q: "q", K_r: "r", K_s: "s", K_t: "t",
-                    K_u: "u", K_v: "v", K_w: "w", K_x: "x", K_y: "y", K_z: "z"}
-
 
 def main():
     """"""
