25d6292ff85e24cd90259b911bf161eee3eaf4e5
MrRobot <olivier.portal@laplateforme.io>
2025-01-23 18:42:05 +0100

    refactor: move function bouton_difficile in separate file

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from pygame.locals import *
 import random
 
 from fonctions.ajouter_mot import ajouter_mot
+from fonctions.bouton_difficile import bouton_difficile
 
 # Initialisation de Pygame
 pygame.init()
@@ -223,37 +224,6 @@ def bouton_arreter():
         fenetre.blit(arreter, (845, 150))
     
     return clic
-
-
-def bouton_difficile(etat_difficulte):
-
-    souris = pygame.mouse.get_pos()
-    bouton_largeur = 170
-    bouton_hauteur = 40
-    bouton = pygame.image.load("image/acier2.jpg")
-    bouton = pygame.transform.scale(bouton, (bouton_largeur, bouton_hauteur))
-
-    # Détection de la souris sur le bouton
-    if 800 <= souris[0] <= 800 + bouton_largeur and 220 <= souris[1] <= 240 + bouton_hauteur:
-        fenetre.blit(bouton, (800, 220))
-        texte = police_survol.render(etat_difficulte, True, BLANC)
-        fenetre.blit(texte, (840, 230))
-
-        # Vérification du clic
-        for evenement in pygame.event.get():
-            if evenement.type == pygame.MOUSEBUTTONDOWN:
-                if etat_difficulte == "normal":
-                    etat_difficulte = "difficile"
-                elif etat_difficulte == "difficile":
-                    etat_difficulte = "facile"
-                elif etat_difficulte == "facile":
-                    etat_difficulte = "normal"
-    else:
-        fenetre.blit(bouton, (800, 220))
-        texte = police.render(etat_difficulte, True, NOIR)
-        fenetre.blit(texte, (840, 230))
-        
-    return etat_difficulte
 
         
 def bouton_ajout():
