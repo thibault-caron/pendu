0278a87bafb554cd0f937db9a0c0e4fae484584e
MrRobot <olivier.portal@laplateforme.io>
2025-01-23 15:37:58 +0100

    fix: ajouter_mot

--- fonctions/ajouter_mot.py
+++ fonctions/ajouter_mot.py
@@ -1,21 +1,21 @@
-    def existence_mot(nouveau_mot):
-        """
-        Vérifie si le mot est déjà présent dans le fichier.
-        :param nouveau_mot: Mot à ajouter au fichier.
-        :return: True si le mot est déja dans le fichier, sinon None.
-        """
-        liste_mots = []
-        with open(fichier, "r") as pendu:
-            for ligne in pendu:
-                unite = ligne[:-1]
-                liste_mots.append(unite)
+def existence_mot(fichier, nouveau_mot):
+    """
+    Vérifie si le mot est déjà présent dans le fichier.
+    :param nouveau_mot: Mot à ajouter au fichier.
+    :return: True si le mot est déja dans le fichier, sinon None.
+    """
+    liste_mots = []
+    with open(fichier, "r") as pendu:
+        for ligne in pendu:
+            unite = ligne[:-1]
+            liste_mots.append(unite)
         for mots in liste_mots:
             if nouveau_mot == mots:
                 return True
 
-    if not existence_mot(mot):
+    if not existence_mot(nouveau_mot):
        with open(fichier, "a") as mots_pendu:
-            mots_pendu.write(f"\n{mot}")
+            mots_pendu.write(f"\n{nouveau_mot}")
 
 
 # fichier_mots = "mots.txt"
